b2bd5ef2d7a483c1a1cbfc1ed748e5f35d0c63f6
Edward_Odey <edwardodey04@gmail.com>
2023-08-10 06:00:27 -0700

    10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+#define MAXLIST 20
+
+int check_cycle(listint_t *list)
+{
+	int i;
+	char *addresses[MAXLIST];
+	listint_t *current;
+
+
+	for (i = 0; i < MAXLIST; i++)
+		addresses[i] = NULL;
+
+	current = list;
+	while (current != NULL)
+	{
+		i = 0;
+		while (addresses[i] != NULL)
+		{
+			if ((char *)current == addresses[i])
+				return (1);
+
+			i++;
+		}
+		addresses[i] = (char *)current;
+		current = current->next;
+	}
+	return (0);
+}
