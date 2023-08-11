a2ca1b22c89eb82b5760ab0a0ed84ba1263a4551
Edward_Odey <edwardodey04@gmail.com>
2023-08-11 15:03:56 -0700

    update 1-calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     print(a, "+", b, "=", add(a, b))
     print(a, "-", b, "=", sub(a, b))
     print(a, "*", b, "=", mul(a, b))
-    print(a, "/", b, "=", div(a, b))
+    print(a, "/", b, "=", round(div(a, b)))
