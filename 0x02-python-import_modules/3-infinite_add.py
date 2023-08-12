23dccfa910b9ea5ace9627066dabb57f4cd80bca
Edward_Odey <edwardodey04@gmail.com>
2023-08-11 17:44:38 -0700

    added 3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    result = 0
+    if len(argv) == 1:
+        print("{:d}".format(result))
+    else:
+        for i in range(1, len(argv)):
+            result += int(argv[i])
+        print("{:d}".format(result))
