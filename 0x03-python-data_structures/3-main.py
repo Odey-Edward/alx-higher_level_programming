1c4461bd6a1df1b178783baed15d879dab6257d6
Edward_Odey <edwardodey04@gmail.com>
2023-08-13 14:39:19 -0700

    added 4-new_in_list.py

--- 0x03-python-data_structures/3-main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
-
-my_list = [1, 2, 3, 4, 5]
-print_reversed_list_integer(my_list)
-print()
-for i in my_list:
-    print(i)
