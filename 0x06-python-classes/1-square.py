0eb34d37c567c3b78e0efb7fe6b3a1d07f12c7f9
Edward_Odey <edwardodey04@gmail.com>
2023-08-29 16:30:33 -0700

    created 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' create a Square class '''
+
+
+class Square:
+    ''' A Square class, no class attribute '''
+
+    def __init__(self, size):
+        '''
+        Args:
+            size (int): Private instance attribute
+
+        '''
+        self.__size = size
