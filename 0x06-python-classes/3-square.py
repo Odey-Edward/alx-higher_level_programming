d15b18f30f8ff8d548053f4b536d41a0c8c9e658
Edward_Odey <edwardodey04@gmail.com>
2023-08-29 18:47:41 -0700

    created: 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+class Square:
+    ''' Defining a Square class '''
+
+    def __init__(self, size=0):
+        ''' Initializing a square attribute
+        Args:
+            size: A private attribute instance
+        '''
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+
+        ''' return the area of the current square '''
+        return (self.__size ** 2)
