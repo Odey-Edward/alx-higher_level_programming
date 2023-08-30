3a00e44069bdd54da099802f39833d56c802ec95
Edward_Odey <edwardodey04@gmail.com>
2023-08-30 03:38:26 -0700

    task-6 updated

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -56,7 +56,7 @@ class Square:
 
     def my_print(self):
         '''prints in stdout the square with the character #'''
-        
+
         if self.__size == 0:
             print()
         else:
