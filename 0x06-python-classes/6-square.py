14d0632b26268702595d312788fbf6dad2c58325
Edward_Odey <edwardodey04@gmail.com>
2023-08-30 03:31:27 -0700

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -58,6 +58,8 @@ class Square:
         '''prints in stdout the square with the character #'''
 
         print()
+        if self._size == 0:
+            print()
         for i in range(self.__size):
             print(" " * self.__position[0], end="")
             print("#" * self.__size)
