a075acffecaa8380bf05129d43b984cf324da4ed
Edward_Odey <edwardodey04@gmail.com>
2023-08-30 03:57:38 -0700

    update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -61,7 +61,7 @@ class Square:
             print()
         else:
             if self.__position[1] > 0:
-                print()
+                print(end=self.__position[1] * "\n")
             for i in range(self.__size):
                 print(" " * self.__position[0], end="")
                 print("#" * self.__size)
