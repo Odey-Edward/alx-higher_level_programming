e2a3046fce9db3b3b02d1d6ec191802e6e77dfa6
Edward_Odey <edwardodey04@gmail.com>
2023-09-05 07:52:58 -0700

    created 0-add_integer.py and a text file

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is a '0-add_integer' module
+
+adds 2 integers and return the result
+"""
+
+
+def add_integer(a, b=98):
+    """return the result of the addition a and b.
+    raise a TypeError if a or b are neither float or integer
+    """
+    if not isinstance(a, (int, float)):
+            raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a, b = int(a), int(b)
+    return (a + b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,11 @@
+**0x07. Python - Test-driven development**
+
+* Python
+* UnitTests
+* TDD
+
+In this project i worked on Python Test-driven development, how to write Docstrings to create tests, UnitTest, basic option flags to create tests, how to find edge cases and much more...
+
+Quote:  
+
+**Don’t trust the user**, always think about all possible edge cases
