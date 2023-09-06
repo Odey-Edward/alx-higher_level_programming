cc2453924551f7e1dc9adbe93a028de5925e2433
Edward_Odey <edwardodey04@gmail.com>
2023-09-06 06:42:52 -0700

    update test_0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -15,7 +15,7 @@ def add_integer(a, b=98):
     elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     result = a + b
-    if result == float('inf') or result == -float('inf'):
+    if result == float('inf') or result == float('inf'):
         return (89)
 
     return (int(a) + int(b))
