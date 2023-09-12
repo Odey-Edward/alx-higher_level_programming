24dc05b23ca24a6781e25ef0f16a624f7ce8d760
Edward_Odey <edwardodey04@gmail.com>
2023-09-12 15:34:50 -0700

    created 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Define a Student class"""
+
+
+class Student:
+    """A student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializing a Student instance variable"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=""):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs:
+            atr_dict = {}
+            for a in attrs:
+                if hasattr(self, a):
+                    atr_dict[a] = getattr(self, a)
+            return (atr_dict)
+        else:
+            return (vars(self))
