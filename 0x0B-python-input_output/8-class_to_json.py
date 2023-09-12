9c49918d5316185bffbcc039460d217f0601613e
Edward_Odey <edwardodey04@gmail.com>
2023-09-12 14:28:40 -0700

    update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """defined class_to_json function"""
 
+
 def class_to_json(obj):
     """ returns the dictionary description with simple
     data structure for JSON serialization of an object"""
