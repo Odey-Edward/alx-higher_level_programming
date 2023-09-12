6bbc1de42fd9628bc85cfcf7b091b1c6ff243d01
Edward_Odey <edwardodey04@gmail.com>
2023-09-12 06:59:13 -0700

    created 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defined the write_file function"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8)
+    and returns the number of characters written"""
+    with open(filename, 'w', encoding='UTF-8') as f:
+        return (f.write(text))
