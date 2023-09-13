0790ff808c4cfc9fcb0b3b55093ef2106567c03c
Edward_Odey <edwardodey04@gmail.com>
2023-09-13 05:36:15 -0700

    created 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Define a Student class"""
+
+
+class Student:
+    """A student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializing a Student instance variable"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=""):
+        """retrieves a dictionary representation of a Student instance"""
+        if type(attrs) == list and all(type(el) == str for el in attrs):
+            atr_dict = {}
+            for a in attrs:
+                if hasattr(self, a):
+                    atr_dict[a] = getattr(self, a)
+            return (atr_dict)
+        else:
+            return (vars(self))
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for attr in list(json):
+            if hasattr(self, attr):
+                setattr(self, attr, json[attr])
