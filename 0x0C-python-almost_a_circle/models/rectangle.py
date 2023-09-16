d2b806f06d94325a41c68278b837f563942a85a7
Edward_Odey <edwardodey04@gmail.com>
2023-09-16 07:31:14 -0700

    added public method display() to the Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,3 +89,8 @@ class Rectangle(Base):
     def area(self):
         """returns the area value of the Rectangle instance"""
         return (self.width * self.height)
+
+    def display(self):
+        """prints in stdout the Rectangle instance with the character #"""
+        for _ in range(self.height):
+            print("#" * self.width)
