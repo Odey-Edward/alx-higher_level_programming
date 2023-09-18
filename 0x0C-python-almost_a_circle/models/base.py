024aae767710b90c322626439cd68b5184d79011
Edward_Odey <edwardodey04@gmail.com>
2023-09-18 04:39:30 -0700

    added from_json_string() method to the Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,3 +35,10 @@ class Base:
 
         with open(filename, "w", encoding="UTF-8") as f:
             f.write(str(cls.to_json_string(cls_list)))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string:
+            return (json.loads(json_string))
+        return ([])
