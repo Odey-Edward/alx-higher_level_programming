9e04dc43fcd4966aac09a756936a9a56f9bf61e6
Edward_Odey <edwardodey04@gmail.com>
2023-10-18 16:01:03 -0700

    added 6-states.sql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- a script that creates the database hbtn_0d_usa and the table cities
+-- (in the database hbtn_0d_usa) on your MySQL server
+-- If the database hbtn_0d_usa already exists, your script should not fail
+-- If the table cities already exists, your script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	state_id INT NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+	name VARCHAR(256) NOT NULL
+);
