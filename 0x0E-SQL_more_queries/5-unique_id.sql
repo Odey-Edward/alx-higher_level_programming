75c7dfcea1b6eebecf2c6fadc6317e90cd267cc9
Edward_Odey <edwardodey04@gmail.com>
2023-10-18 15:01:54 -0700

    created 5-unique_id.sql

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- a script that creates the table unique_id on your MySQL server.
+-- If the table unique_id already exists, your script should not fail
+CREATE TABLE IF NOT EXISTS unique_id(
+	id INT UNIQUE DEFAULT 1,
+	name VARCHAR(256)
+);
