90af6a2221f4392f9d712a3aaa49cc63cc637a43
Edward_Odey <edwardodey04@gmail.com>
2023-10-19 09:48:19 -0700

    updated 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,5 @@
 -- a script that lists all cities contained in the database hbtn_0d_usa
-SELECT id, name, name FROM cities NATURAL JOIN states;
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states
+ON state_id = states.id;
