eef185e2ce27b3615cab486a767d099011358820
Edward_Odey <edwardodey04@gmail.com>
2023-11-13 03:22:40 -0800

    create 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,8 +4,6 @@ const { argv } = require('node:process');
 
 if (argv[2] == null) {
   console.log('No argument');
-} else if (argv[3] != null) {
-  console.log('Arguments found');
 } else {
-  console.log('Argument found');
+  console.log(argv[2]);
 }
