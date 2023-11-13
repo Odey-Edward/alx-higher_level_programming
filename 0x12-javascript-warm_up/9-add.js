e8f3612536ddc829cf5e9be0cc6320f8bc0e3291
Edward_Odey <edwardodey04@gmail.com>
2023-11-13 05:25:47 -0800

    create 9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+const num1 = Number(argv[2]);
+const num2 = Number(argv[3]);
+
+function add (a, b) {
+  console.log(a + b);
+}
+
+add(num1, num2);
