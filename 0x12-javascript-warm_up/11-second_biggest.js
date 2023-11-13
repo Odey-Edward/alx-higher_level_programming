4289d06e160f4009c14616b89b32e69ae8c36461
Edward_Odey <edwardodey04@gmail.com>
2023-11-13 10:46:48 -0800

    create 11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+const args = argv.slice(2);
+let high = 0;
+let secondHigh = 0;
+
+if (argv.length <= 3) {
+  console.log(0);
+  process.exit();
+}
+
+for (let i = 0; i < (args.length); i++) {
+  if (args[i] > high) {
+    high = args[i];
+  }
+}
+
+for (let i = 0; i < (args.length); i++) {
+  if (args[i] < high && args[i] > secondHigh) {
+    secondHigh = args[i];
+  }
+}
+
+console.log(secondHigh);
