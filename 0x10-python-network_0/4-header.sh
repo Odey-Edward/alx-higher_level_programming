980f0f74eb84bcbf7774149163ecbd5137e51443
Edward_Odey <edwardodey04@gmail.com>
2023-11-30 12:39:24 -0800

    update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#sends a GET request to the URL, and displays the body of the response
-curl -s "$1?X-School-User-Id=98"
+#sends a GET request to the URL, set custom header and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
