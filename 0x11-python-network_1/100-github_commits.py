5e90a7a26d1a8296d88e776e42b0079234da39e5
Edward_Odey <edwardodey04@gmail.com>
2023-12-01 14:49:12 -0800

    create 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+list 10 commits (from the most recent to oldest) form a repository
+"""
+from sys import argv
+import requests
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/repos/'\
+            + argv[2] + '/' + argv[1] + '/commits'
+    header = {
+            "Accept": "application/vnd.github+json",
+            "Authorization": argv[2],
+            "X-GitHub-Api-Version": "2022-11-28"
+            }
+
+    response = requests.get(url, headers=header)
+
+    array = response.json()
+
+    for i in range(11):
+        print(
+            array[i].get('sha') + ': '
+            + array[i].get('commit')['author']['name'])
