c744bf45f59cb1e02ca84168730aed77c8973978
Edward_Odey <edwardodey04@gmail.com>
2023-12-01 11:38:12 -0800

    create 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+from sys import argv
+import requests
+
+if __name__ == '__main__':
+    values = {}
+
+    if len(argv) == 1:
+        values['q'] = ""
+    else:
+        values['q'] = argv[1]
+
+    try:
+        res = requests.post('http://0.0.0.0:5000/search_user', data=values)
+        json_body = res.json()
+
+        if not json_body:
+            print('No result')
+        else:
+            print('{[]} {}'.format(json_body.get('id'), json_body.get('name')))
+    except ValueError:
+        print('Not a valid JSON')
