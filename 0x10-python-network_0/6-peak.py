0361f83cb7b060b0ae0957a515c47bbe4a61de3b
Edward_Odey <edwardodey04@gmail.com>
2023-11-30 16:03:35 -0800

    6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""function find_peak"""
+
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return
+
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    return recursive(0, len(list_of_integers) - 1, list_of_integers)
+
+def recursive(start, end, list_items):
+
+    if start >= end:
+            return 0
+
+    _len = end / 2
+
+    value1 = recursive(start, _len, list_items)
+    value2 = recursive(_len, end, list_items)
+
+    if value1 > value2:
+        return value1
+
+    return value2
+
+
